657beb312f00c31eb7cf4a6e8cd0de061219842b
maazsami <maazsamifaisal@gmail.com>
2021-09-21 13:41:23 -0700

    Loops

---
+++ factorial program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace factorial_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1;
+
+            int fact2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <=fact2; i++)
+            {
+                fact = fact * i;
+            }
+
+            Console.WriteLine(fact);
+
+            Console.ReadKey();
+        }
+    }
+}
